c3432cb86051784ad16c2d3710efbe4b75fa8247
Quan <lhongquan.1998@gmail.com>
2024-11-01 17:07:12 +0700

    init snake components and firestore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SnakeGame from './components/SnakeGame.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Mấy con gà UIT Nghề và Nghiệp
+      <h1>Snake Game</h1>
+      <SnakeGame />
     </div>
   );
 }
